e0c7e4a50134cfa079413b219d4636a94a48f9c0
Jonathan Levin <jonmlevin@outlook.com>
2019-07-31 22:10:29 +0300

    pivot to correct solution (banner.p file in source)

--- pc05_peak/main.py
+++ pc05_peak/main.py
@@ -1,64 +1,37 @@
+import requests
+from bs4 import BeautifulSoup
 import pickle
 import re
+import urllib3
+import os
 
-# last_challange_dir = '../pc04_linkedlist/'
-# with open(f'./nothings', 'w+') as new:
-#     with open(f'{last_challange_dir}/nothings_0', 'r') as file:
-#         new.write(file.read())
-#     with open(f'{last_challange_dir}/nothings_1', 'r') as file:
-#         new.write(file.read())
+url = "http://www.pythonchallenge.com/pc/def/peak.html"
+filename = 'banner.p'
 
-r"""
-Hypotheses:
-1. all pickle bstrings start with b'\x80\x0<V>' where <V> is the version num
-2. pickle bstrings (entire) end differently depending on the root object
-3. for strings- they will end 'q\x00', directly after the last character
-4. for arrays- they will end
-"""
+def save_pickle_from_url(url, filename):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    tag = soup.find('peakhell')
+    file = tag['src']
 
-nothings = []
+    url_directory = os.path.split(url)[0]
+    url_file = f'{url_directory}/{file}'
+    response = requests.get(url_file)
+    with open(filename, 'wb+') as file:
+        file.write(response.content)
 
-def test_pickle(objects, version):
-    path = f'./pickle_test_v{version}'
-    with open(path, 'w') as file:
-        for obj in objects:
-            obj_str = f'"{obj}"' if type(obj) is str else obj
-            file.write(f'{obj_str}\t->\t')
-            file.write(str(pickle.dumps(obj, protocol=version)))
-            file.write('\n')
-
-def get_nothings(path: str) -> [int]:
-    text = ""
-    with open(path, 'r') as file:
-        text = file.read()
-    return [int(nothing) for nothing in text.split('\n') if len(nothing) > 0]
-
-
-def test_all():
-    versions = range(0, 5)
-    objects = [
-        "{}",
-        "a",
-        "asflkjnqwrt",
-        {'a': 'a'},
-        {1: 1},
-        [1, 2, 3],
-        {'a': [1, 2, 3]},
-        ['a', 'b']
-    ]
-    for version in versions:
-        test_pickle(objects, version)
-
+def get_pickle_from_file(filename):
+    with open(filename, 'rb') as file:
+        return file.read()
 
 if __name__ == '__main__':
-    test_all()
-    exit(0)
-    print()
-    byts = None
-    with open('peakhell.jpg', 'rb') as pic:
-        byts = pic.read()
-        # locations = [x.start() for x in ]
-        print(re.findall(b'\x80\x04.{30}', byts))
+    # save_pickle_from_url(url, filename)
+    data = get_pickle_from_file(filename)
 
-    nothings = get_nothings('nothings')
-    # print(min(nothings), max(nothings))
+    parsed = pickle.loads(data)
+    processed = [[(tup[0], chr(tup[1]))for tup in p] for p in parsed]
+    for p in parsed:
+        for c, count in p:
+            print(c * count, end='')
+        print()
+
